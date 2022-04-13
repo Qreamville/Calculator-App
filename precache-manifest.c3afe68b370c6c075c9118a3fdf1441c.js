3f3ed695c3b08ce3dbec7cd3b7e6c9a85d0ef0d1
Qreamville <Oyeleyebalikis@yahoo.com>
2022-04-12 21:28:12 -1200

    Updates

---
+++ precache-manifest.c3afe68b370c6c075c9118a3fdf1441c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25362e7a38e7ffc2314d",
+    "url": "/Calculator-App/static/css/main.02adc8d7.chunk.css"
+  },
+  {
+    "revision": "25362e7a38e7ffc2314d",
+    "url": "/Calculator-App/static/js/main.839f108b.chunk.js"
+  },
+  {
+    "revision": "b90a1221529361e7343c",
+    "url": "/Calculator-App/static/js/runtime~main.2afaf4f3.js"
+  },
+  {
+    "revision": "a708623d981d8cb4fa99",
+    "url": "/Calculator-App/static/js/2.2364c2b6.chunk.js"
+  },
+  {
+    "revision": "8feda4f5e44ebbefbd1ec93ef708722f",
+    "url": "/Calculator-App/index.html"
+  }
+];
